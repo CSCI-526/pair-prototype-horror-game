9dd549445ac8ad1dfb2c9bfcf490850a12d5777f
AngeliaZddl <78582332+AngeliaZddl@users.noreply.github.com>
2024-10-02 14:47:35 -0700

    Sensor and Ghost moving

--- Prototype/Assets/Scripts/RandomBouncingMovement.cs
+++ Prototype/Assets/Scripts/RandomBouncingMovement.cs
@@ -1,12 +1,22 @@
+using UnityEditor;
 using UnityEngine;
 
 public class RandomBouncingMovement : MonoBehaviour
 {
     public float moveSpeed = 5f; // Speed of movement
     private Vector3 moveDirection;
+    private Vector3 originalPosition;
+
+    private float minX = -50;
+    private float maxX = 50;
+    private float minZ = -50;
+    private float maxZ = 50;
+    private float minY = 0;
+    private float maxY = 2;
 
     void Start()
     {
+        originalPosition = transform.position;
         // Initialize a random direction for movement
         GetRandomDirection();
     }
@@ -15,6 +25,14 @@ public class RandomBouncingMovement : MonoBehaviour
     {
         // Move the object
         transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+        if (transform.position.x < minX || transform.position.x > maxX || 
+            transform.position.z < minZ || transform.position.z > maxZ ||
+            transform.position.y < minY || transform.position.y > maxY)
+        {   
+            transform.position = originalPosition;
+            GetRandomDirection();
+        }
     }
 
     void GetRandomDirection()
@@ -27,7 +45,8 @@ public class RandomBouncingMovement : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
         // Check if the object collided with a wall
-        if (collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Ghost"))
+        if (collision.gameObject.CompareTag("Wall") || 
+            collision.gameObject.CompareTag("Ghost"))
         {
             // Reflect the direction based on the normal of the collision
             moveDirection = Vector3.Reflect(moveDirection, collision.contacts[0].normal);
